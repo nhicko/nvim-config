661557e7bdb1b2976fb4a3548476289a760d8307
nhicko <npmcatipon@gmail.com>
2024-08-14 10:10:37 +0800

    entry updates

--- bpi.lua
+++
@@ -1,9 +0,0 @@
-require("luasnip.session.snippet_collection").clear_snippets("bpi")
-local ls = require("luasnip")
-local s = ls.snippet
-local i = ls.insert_mode
-
-local fmt = require("luasnip.extras.fmt").fmt
-ls.add_snippets("bpi", {
-	s("tt", fmt("filename: {}\nsource: {}", { i(1), i(0) })),
-})
