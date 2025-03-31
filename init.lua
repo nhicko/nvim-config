22a232eca0e7fdaab450041dbc0841331e694c52
niko <npmcatipon@gmail.com>
2025-03-31 14:55:19 +0800

    updated init.lua commented yank to clipboard command

--- init.lua
+++ init.lua
@@ -1,17 +1,4 @@
 require 'config.lazy'
 require 'config.set'
 require 'config.keymap'
-vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    -- Check if the 'clipboard' is available and yank was not empty
-    if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
-      local yank_content = vim.fn.getreg(vim.v.event.regname)
-      if yank_content and #yank_content > 0 then
-        -- Use clip.exe to copy the yanked content to clipboard
-        local handle = io.popen('echo ' .. vim.fn.shellescape(yank_content) .. ' | /mnt/c/Windows/system32/clip.exe')
-        handle:close()
-      end
-    end
-  end
-})
+-- autocmd TextYankPost * if v:event.operator ==# 'y' | call system('/mnt/c/Windows/System32/clip.exe',@0) | endif
