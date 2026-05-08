84ccb684253f5091b0d96c2ba323b0f449e628ff
npmcatipon <npmcatipon@bpi.com.ph>
2026-05-08 08:45:15 +0800

    added nvim support for org-mode

--- init.lua
+++ init.lua
@@ -94,6 +94,16 @@ require('blink.cmp').setup({
 --        completion.keyword = { range = 'prefix' }
 })
 
+vim.pack.add({
+  { src = 'https://github.com/nvim-orgmode/orgmode'}
+})
+require('orgmode').setup({
+  org_agenda_files = '~/orgfiles/**/*',
+  org_default_notes_file = '~/orgfiles/refile.org',
+})
+-- Experimental LSP support
+vim.lsp.enable('org')
+
 vim.lsp.enable ({ 'jdtls',
                   'lua_ls',
                   'bash-language-server',
