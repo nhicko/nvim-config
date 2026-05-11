22bb16826e4cb186764576f591dcc97839453401
npmcatipon <npmcatipon@bpi.com.ph>
2026-05-11 19:47:10 +0800

    rebase: init.lua included all functionalities for orgmode

--- init.lua
+++ init.lua
@@ -28,7 +28,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 -- for WSL1, disable if not using WSL1
-vim.api.nvim_create_autocmd("TextYankPost",{callback=function() vim.fn.system("clip.exe",vim.fn.getreg('"')) end})
+-- vim.api.nvim_create_autocmd("TextYankPost",{callback=function() vim.fn.system("clip.exe",vim.fn.getreg('"')) end})
 
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', '<S-n>', '<S-n>zz')
@@ -98,11 +98,10 @@ vim.pack.add({
   { src = 'https://github.com/nvim-orgmode/orgmode'}
 })
 require('orgmode').setup({
+  org_todo_keywords = {'TODO', 'IN_PROGRESS', 'WAITING', '|', 'DONE', 'CANCELLED'},
   org_agenda_files = '~/orgfiles/**/*',
   org_default_notes_file = '~/orgfiles/refile.org',
 })
--- Experimental LSP support
-vim.lsp.enable('org')
 
 -- Function to insert formatted date
 local function insert_custom_date()
@@ -120,5 +119,8 @@ vim.lsp.enable ({ 'jdtls',
                   'lua_ls',
                   'bash-language-server',
                   'markdown',
-                  'rust-analyzer'
+                  'rust-analyzer',
+		  'org'
                 })
+
+    
